8b5d5d1a17ca51cc19dc3bf4aa16cb375ca4ab78
Mohamed Ahmed <77528820+momo46060@users.noreply.github.com>
2024-12-10 09:56:45 +0200

    Initial commit

---
+++ src/main/kotlin/com/icare/ICareApplication.kt
@@ -0,0 +1,11 @@
+package com.icare
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ICareApplication
+
+fun main(args: Array<String>) {
+    runApplication<ICareApplication>(*args)
+}
---
+++ src/main/kotlin/com/icare/di/AppModule.kt
@@ -0,0 +1,46 @@
+package com.icare.di
+
+import com.google.auth.oauth2.GoogleCredentials
+import com.google.firebase.FirebaseApp
+import com.google.firebase.FirebaseOptions
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.jdbc.DataSourceBuilder
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.env.Environment
+import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.util.ResourceUtils
+import java.io.FileInputStream
+import javax.sql.DataSource
+
+
+@Configuration
+class AppModule {
+    @Autowired
+    private lateinit var env: Environment
+    @Bean
+    fun fireBaseInitializing(): FirebaseApp {
+        val serviceAccount = FileInputStream(ResourceUtils.getFile("servicekey.json").path)
+        val options = FirebaseOptions.Builder()
+            .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+            .build()
+
+        return FirebaseApp.initializeApp(options)
+    }
+
+    @Bean
+    fun database(): DataSource {
+        val dataSourceBuilder = DataSourceBuilder.create()
+        dataSourceBuilder.driverClassName(env.getProperty("spring.datasource.driverClassName")) // Replace if needed based on your driver
+        dataSourceBuilder.url(env.getProperty("spring.datasource.url"))
+        dataSourceBuilder.username(env.getProperty("spring.datasource.username"))
+        dataSourceBuilder.password(env.getProperty("spring.datasource.password"))
+        return dataSourceBuilder.build()
+    }
+
+    @Bean
+    fun iCareJdbcTemplate(database: DataSource?): JdbcTemplate {
+        return JdbcTemplate(database!!)
+    }
+
+}
