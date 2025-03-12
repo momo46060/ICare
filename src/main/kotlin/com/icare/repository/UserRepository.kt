5e1a075d53c2dd9eeb3b5dad8e924255bb55d54c
Mohamed Ahmed <77528820+momo46060@users.noreply.github.com>
2025-03-12 09:22:55 +0200

    Use ResponseModel in patient_register api

--- src/main/kotlin/com/icare/repository/UserRepository.kt
+++ src/main/kotlin/com/icare/repository/UserRepository.kt
@@ -1,12 +1,13 @@
 package com.icare.repository
 
 import com.icare.model.PatientModel
+import com.icare.model.ResponseModel
 import com.icare.model.Users
 
 
 interface UserRepository {
 
-    fun registerPatient(patient: PatientModel): Boolean
-    fun insertUser(users: Users): Boolean
+    fun registerPatient(patient: PatientModel): ResponseModel
+    fun insertUser(user: Users): Boolean
 
 }
