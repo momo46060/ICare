267d758293492b9567a28ff11a2c6a45836d54c9
Saber <saberfarouk_2000@gmail.com>
2025-04-16 12:35:43 +0200

    Edit API Doctor model add from_time and to_time

--- src/main/kotlin/com/icare/model/Doctor.kt
+++ src/main/kotlin/com/icare/model/Doctor.kt
@@ -5,7 +5,8 @@ import org.springframework.boot.availability.ApplicationAvailability
 data class Doctor(
     val doctorID:String="",
     val specializationID:String="",
-    val doctorAvailability:String="",
+    val fromTime: Long = 0,
+    val toTime: Long = 0,
     val clincId:String="",
 
-)
+    )
