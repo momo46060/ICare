3f7d414364247a22ce598776389e1203d44f4771
Saber <saberfarouk_2000@gmail.com>
2025-04-16 12:39:58 +0200

    update and seprate ClinicController.kt ClinicRepositry.kt ClinicRepositryImpl.kt ClinicService.kt ClinicServiceImpl.kt

---
+++ src/main/kotlin/com/icare/controller/ClinicController.kt
@@ -0,0 +1,28 @@
+package com.icare.controller
+
+import com.icare.model.ClinicModel
+import com.icare.model.ResponseModel
+import com.icare.service.ClinicService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/clinicApi")
+class ClinicController {
+    @Autowired
+    lateinit var service: ClinicService
+
+    @PostMapping("/add_clinic")
+    fun addClinic(@RequestBody clinicModel: ClinicModel): ResponseModel {
+        return service.addClinic(clinicModel)
+    }
+
+    @PostMapping("/getclinics")
+    fun getClinics (@RequestBody token: String): ResponseModel {
+        return service.getClinics(token)
+    }
+
+}
---
+++ src/main/kotlin/com/icare/service/ClinicServiceImpl.kt
@@ -0,0 +1,42 @@
+package com.icare.service
+
+import com.icare.model.ClinicModel
+import com.icare.model.ResponseModel
+import com.icare.repository.ClinicRepositry
+import com.icare.repository.PharmacyRepository
+import com.icare.utils.EMPTY_LIST
+import com.icare.utils.FAILED
+import com.icare.utils.INVALID_TOKEN
+import com.icare.utils.OK
+import com.icare.utils.getUid
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+
+@Service
+class ClinicServiceImpl : ClinicService{
+    @Autowired
+    lateinit var repository: ClinicRepositry
+
+    override fun addClinic(clinic: ClinicModel): ResponseModel {
+        return ResponseModel(repository.addClinic(clinic),"")
+    }
+
+
+
+    override fun getClinics(token:String): ResponseModel {
+        try {
+            if(getUid(token) == null) {
+                return ResponseModel(status=INVALID_TOKEN)
+            }else if (repository.getClinics().isEmpty()){
+                return ResponseModel(status= EMPTY_LIST)
+            }else{
+                return ResponseModel(status=OK,data = repository.getClinics())
+            }
+        }catch (e:Exception){
+            e.printStackTrace()
+            return ResponseModel(status= FAILED)
+        }
+
+
+    }
+}
