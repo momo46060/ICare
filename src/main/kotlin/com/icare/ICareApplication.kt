daf5673d005462ac765f207aedbf8bd5666deed8
Saber <saberfarouk_2000@gmail.com>
2025-04-17 13:05:56 +0200

    refactor: clean up formatting in ICareApplication.kt

--- src/main/kotlin/com/icare/ICareApplication.kt
+++ src/main/kotlin/com/icare/ICareApplication.kt
@@ -3,10 +3,10 @@ package com.icare
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-
+@SpringBootApplication
 class ICareApplication
 
 fun main(args: Array<String>) {
-    runApplication<ICareApplication>(*args)
+     runApplication<ICareApplication>(*args)
 
 }
