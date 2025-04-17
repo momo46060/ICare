644bcc449a4160b85bc417a72571a9698f6f7102
Saber <saberfarouk_2000@gmail.com>
2025-04-17 13:10:17 +0200

    Clean up unused imports in ClinicService
    Removed unused imports for PatientModel, ClinicRepositry, PharmacyRepository, and @Autowired. This improves code readability and reduces unnecessary dependencies.

--- src/main/kotlin/com/icare/service/ClinicService.kt
+++ src/main/kotlin/com/icare/service/ClinicService.kt
@@ -1,11 +1,7 @@
 package com.icare.service
 
 import com.icare.model.ClinicModel
-import com.icare.model.PatientModel
 import com.icare.model.ResponseModel
-import com.icare.repository.ClinicRepositry
-import com.icare.repository.PharmacyRepository
-import org.springframework.beans.factory.annotation.Autowired
 
 
 interface ClinicService {
