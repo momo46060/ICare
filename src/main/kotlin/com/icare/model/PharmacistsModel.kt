9daa81bb34dd73239c9f3099d83698a3dbb6f903
Ali Mansour <dev.ali.mansour@gmail.com>
2025-04-19 19:25:44 +0200

    Refactor PharmacistsModel to improve property naming and remove unused fields

--- src/main/kotlin/com/icare/model/PharmacistsModel.kt
+++ src/main/kotlin/com/icare/model/PharmacistsModel.kt
@@ -2,18 +2,12 @@ package com.icare.model
 
 class PharmacistsModel(
     val token: String = "0",
-    val roleID: Int = 0,
+    val pharmacistID :String="",
     val fName:String="",
     val lName:String="",
     val email:String="",
-    val birthDate:String="",
-    val gender:String="",
     val isActive:Boolean=true,
     val phoneNumber: String = "",
-    val address: String = "",
-    val nationalId:String="",
-    val PharmacistID :Int =0,
-    val PharmacyId :String = ""
-
-
+    val pharmacyId :Long=0,
+    val profilePicture: String = "",
 )
