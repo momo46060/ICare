64556a04006deec06954cc68a143238272010bb8
Saber <saberfarouk_2000@gmail.com>
2025-04-19 18:37:10 +0200

    Refactor ImagingCentersRepository to improve SQL handling
    Rewrote the SQL merge query to use parameterized statements, enhancing security and maintainability. Updated method logic to return standardized status codes and handle exceptions more effectively. Corrected inconsistencies in column and variable naming for clarity and consistency.

--- src/main/kotlin/com/icare/repository/ImagingCentersRepositoryImpl.kt
+++ src/main/kotlin/com/icare/repository/ImagingCentersRepositoryImpl.kt
@@ -1,49 +1,63 @@
 package com.icare.repository
 
-import com.icare.model.ClinicModel
 import com.icare.model.ImagingCentersModel
-import com.icare.utils.getUid
+import com.icare.utils.FAILED
+import com.icare.utils.OK
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.stereotype.Repository
 
 @Repository
-class ImagingCentersRepositoryImpl: ImagingCentersRepositry {
+class ImagingCentersRepositoryImpl : ImagingCentersRepositry {
     @Autowired
     lateinit var iCareJdbcTemplate: JdbcTemplate
     override fun insertImagingCenter(ImagingCenters: ImagingCentersModel): Short {
-                val sqlMerge = """
-    MERGE INTO Patients AS target
-    USING (VALUES ('${ImagingCenters.CenterID}', '${ImagingCenters.CenterName}','${ImagingCenters.Phone}',
-     '${ImagingCenters.address}', '${ImagingCenters.type}'))
-        AS source ([Patient_ID], [ChronicDiseases], [CurrentMedications], [Allergies], [PastSurgeries], [Weight])
-    ON target.[CenterID] = source.[CenterID]
-    WHEN MATCHED THEN
-        UPDATE SET
-            target.[CenterID] = source.[CenterID],
-            target.[CenterName] = source.[CenterName],
-            target.[Phone] = source.[Phone],
-            target.[address] = source.[address],
-            target.[type] = source.[type]
-    WHEN NOT MATCHED BY TARGET THEN
-        INSERT ([CenterID], [CenterName], [Phone], [address], [type]
-        VALUES (source.[CenterID], source.[CenterName], source.[Phone], source.[address], source.[type]);
- """.trimIndent()
-                iCareJdbcTemplate.update(sqlMerge)
-        return 0
+
+        val sql = """
+            MERGE INTO Lab_Imaging_Centers AS target
+            USING (
+                SELECT ? AS CenterID, ? AS CenterName, ? AS Phone, ? AS Address, ? AS CenterType
+            ) AS source
+            ON target.Center_ID = source.CenterID
+            WHEN MATCHED THEN
+                UPDATE SET
+                    target.CenterName = source.CenterName,
+                    target.Phone = source.Phone,
+                    target.Address = source.Address,
+                    target.CenterType = source.CenterType
+            WHEN NOT MATCHED BY TARGET THEN
+                INSERT (CenterName, Phone, Address, CenterType)
+                VALUES (source.CenterName, source.Phone, source.Address, source.CenterType);
+        """.trimIndent()
+
+        try {
+            iCareJdbcTemplate.update(
+                sql,
+                ImagingCenters.centerID,
+                ImagingCenters.centerName,
+                ImagingCenters.phone,
+                ImagingCenters.address,
+                ImagingCenters.centerType
+            )
+            return OK
+        } catch (e: Exception) {
+            println(e.stackTrace)
+            println(e.message)
+            return FAILED
+        }
     }
 
     override fun getImagingCenters(): List<ImagingCentersModel> {
-        val sql="""
+        val sql = """
             select *  from Lab_Imaging_Centers
         """.trimIndent()
-        return iCareJdbcTemplate.query(sql)  { rs, _ ->
+        return iCareJdbcTemplate.query(sql) { rs, _ ->
             ImagingCentersModel(
-                CenterID = rs.getInt("CenterID"),
-                CenterName = rs.getString("CenterName"),
-                Phone = rs.getString("Phone"),
+                centerID = rs.getInt("Center_ID"),
+                centerName = rs.getString("CenterName"),
+                phone = rs.getString("Phone"),
                 address = rs.getString("address"),
-                type = rs.getInt("type")
+                centerType = rs.getInt("CenterType")
 
             )
         }
