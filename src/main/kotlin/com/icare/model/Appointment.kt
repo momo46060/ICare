b0cb405b2e03f414cb2cf762c872c240cb7f848a
Saber <saberfarouk_2000@gmail.com>
2025-04-20 16:39:05 +0200

    Add patientId to Appointment model
    Introduced a new field `patientId` to the `Appointment` model to store the patient's identifier. Commented out the unused `status` field, as it has been replaced by `statusId`. These changes improve clarity and align the model with the updated requirements.

--- src/main/kotlin/com/icare/model/Appointment.kt
+++ src/main/kotlin/com/icare/model/Appointment.kt
@@ -3,9 +3,11 @@ package com.icare.model
 data class Appointment(
     //get PatientIdFomToken
     val token: String = "",//
+    val patientId: String = "",
+
     val doctorId: String="",//
     val appointmentTime: Long=0,//
-    val status: Int = 0,//
+    //val status: Int = 0,//
     val appointmentId: Long=0,//
     val statusId: Short=0,//
     val doctorSpecialty: String,//
