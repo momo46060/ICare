d3f69cdb257da23d5877c57294829b06e12dfca2
Saber <saberfarouk_2000@gmail.com>
2025-04-22 14:34:07 +0200

    Add `rating` and `price` fields to DoctorModel
    The `DoctorModel` class now includes `rating` and `price` fields to better represent doctor-related data. This enhancement supports future features requiring these attributes.

--- src/main/kotlin/com/icare/model/DoctorModel.kt
+++ src/main/kotlin/com/icare/model/DoctorModel.kt
@@ -12,5 +12,8 @@ data class DoctorModel(
     val toTime:Long=0,
     val clinicId:String="",
     val phoneNumber: String = "",
+    val rating:Double=0.0,
+    val price:Double=0.0,
     val profilePicture:String="",
+
 )
