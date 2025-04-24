6210ba54f84b244cd3866441b6bd777f59430419
Ali Mansour <dev.ali.mansour@gmail.com>
2025-04-24 14:22:33 +0200

    Add INVALID_USER constant to Const.kt and format existing constants

--- src/main/kotlin/com/icare/utils/Const.kt
+++ src/main/kotlin/com/icare/utils/Const.kt
@@ -1,7 +1,8 @@
 package com.icare.utils
 
 const val OK:Short=0;
-const val INVALID_TOKEN:Short=1;
-const val DUPLICATE_USER:Short=2;
-const val FAILED:Short=3;
-const val EMPTY_LIST:Short=4;
+const val INVALID_TOKEN:Short=1
+const val DUPLICATE_USER:Short=2
+const val FAILED:Short=3
+const val EMPTY_LIST:Short=4
+const val INVALID_USER:Short = 5
