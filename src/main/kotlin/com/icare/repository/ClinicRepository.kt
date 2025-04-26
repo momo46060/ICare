c4f2b13f25ff71aecc5fb5ba117cc34e9b9ee723
Ali Mansour <dev.ali.mansour@gmail.com>
2025-04-26 16:34:30 +0300

    Add getDoctorSchedule method to ClinicRepository for retrieving doctor's schedule

--- src/main/kotlin/com/icare/repository/ClinicRepository.kt
+++ src/main/kotlin/com/icare/repository/ClinicRepository.kt
@@ -3,12 +3,14 @@ package com.icare.repository
 import com.icare.model.ClinicModel
 import com.icare.model.ConsultationModel
 import com.icare.model.DoctorModel
+import com.icare.model.DoctorSchedule
 import org.apache.catalina.core.AprStatus
 
 interface ClinicRepository {
     fun addClinic(clinic:ClinicModel):Short
     fun getClinics (): List<ClinicModel>
     fun getDoctors(): List<DoctorModel>
+    fun getDoctorSchedule(uid:String): DoctorSchedule
     fun consultation(consultation: ConsultationModel): Short
     fun getConsultationsByPrescriptionStatus(status: Short): List<ConsultationModel>
     fun getConsultationsByLabTestStatus(status: Short): List<ConsultationModel>
