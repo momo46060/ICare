601ce3eba0ab67783d51c3efcada39b7d93dc6c1
Ali Mansour <dev.ali.mansour@gmail.com>
2025-04-26 16:33:59 +0300

    Refactor AppointmentRepositoryImpl and AppointmentsRepository for code consistency and clarity

--- src/main/kotlin/com/icare/repository/AppointmentsRepository.kt
+++ src/main/kotlin/com/icare/repository/AppointmentsRepository.kt
@@ -5,5 +5,5 @@ import com.icare.model.Appointment
 interface AppointmentsRepository {
     fun insertAppointments(appointment: Appointment): Short
     fun  getPatientAppointments(token: String): List<Appointment>
-    fun getAppointmentsByStatus(status: Int): List<Appointment>
+    fun getAppointmentsByStatus(status: Short): List<Appointment>
 }
