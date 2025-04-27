3f0ebcb146e998a47d253b004788ce39b3ff167b
Ali Mansour <dev.ali.mansour@gmail.com>
2025-04-27 03:04:33 +0300

    Remove unused imports and simplify response handling in ImagingCentersServiceImpl

--- src/main/kotlin/com/icare/service/ImagingCentersServiceImpl.kt
+++ src/main/kotlin/com/icare/service/ImagingCentersServiceImpl.kt
@@ -1,11 +1,8 @@
 package com.icare.service
 
-import com.icare.model.ClinicModel
 import com.icare.model.ImagingCentersModel
 import com.icare.model.ResponseModel
 import com.icare.repository.ImagingCentersRepositry
-import com.icare.repository.PharmacyRepository
-import com.icare.utils.EMPTY_LIST
 import com.icare.utils.FAILED
 import com.icare.utils.INVALID_TOKEN
 import com.icare.utils.OK
@@ -27,8 +24,6 @@ class ImagingCentersServiceImpl: ImagingCentersService {
         try {
             if(getUid(token) == null) {
                 return ResponseModel(status=INVALID_TOKEN)
-            }else if (repository.getImagingCenters().isEmpty()){
-                return ResponseModel(status= EMPTY_LIST, data = listOf<ImagingCentersModel>())
             }else{
                 return ResponseModel(status=OK,data = repository.getImagingCenters())
             }
