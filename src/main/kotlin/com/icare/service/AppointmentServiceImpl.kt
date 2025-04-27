3f276c89397de4e50894e3c763dafe5b227ad0a1
Ali Mansour <dev.ali.mansour@gmail.com>
2025-04-27 03:05:35 +0300

    Remove EMPTY_LIST handling from AppointmentServiceImpl
    Simplified the logic in AppointmentServiceImpl by removing checks for empty lists and associated `EMPTY_LIST` status responses. The service now assumes that the repository handles empty data cases appropriately.

--- src/main/kotlin/com/icare/service/AppointmentServiceImpl.kt
+++ src/main/kotlin/com/icare/service/AppointmentServiceImpl.kt
@@ -5,7 +5,6 @@ package com.icare.service
 import com.icare.model.Appointment
 import com.icare.model.ResponseModel
 import com.icare.repository.AppointmentsRepository
-import com.icare.utils.EMPTY_LIST
 import com.icare.utils.INVALID_TOKEN
 import com.icare.utils.OK
 import com.icare.utils.getUid
@@ -23,8 +22,6 @@ class AppointmentServiceImpl: AppointmentService {
     override fun getPatientAppointments(token: String): ResponseModel {
         if (getUid(token) == null) {
             return ResponseModel(status= INVALID_TOKEN, message = "Unauthorized")
-        }else if (repository.getPatientAppointments(getUid(token)!!).isEmpty()){
-            return ResponseModel(status= EMPTY_LIST, data = listOf<Appointment>())
         }else{
             return ResponseModel(status=OK,data = repository.getPatientAppointments(getUid(token)!!))
         }
@@ -33,12 +30,8 @@ class AppointmentServiceImpl: AppointmentService {
     override fun getAppointmentsByStatus(status: Short,token: String): ResponseModel {
         if (getUid(token) == null) {
             return ResponseModel(status= INVALID_TOKEN, message = "Unauthorized")
-        }else if (repository.getAppointmentsByStatus(status).isEmpty()){
-            return ResponseModel(status= EMPTY_LIST, data = listOf<Appointment>())
         }else{
             return ResponseModel(status=OK,data = repository.getAppointmentsByStatus(status))
         }
     }
-
-
 }
