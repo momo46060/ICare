d5d2109e7d3e139a9d179aa75c6d013ce1f3ee0f
Ali Mansour <dev.ali.mansour@gmail.com>
2025-04-27 03:05:42 +0300

    Remove unused imports in UserService
    Cleaned up unused imports to improve code readability and maintainability. This reduces unnecessary dependencies and ensures the file remains streamlined.

--- src/main/kotlin/com/icare/service/UserSevice.kt
+++ src/main/kotlin/com/icare/service/UserSevice.kt
@@ -1,17 +1,13 @@
 package com.icare.service
 
 import com.icare.model.CenterStaffModel
-import com.icare.model.ClinicModel
 import com.icare.model.DoctorModel
 import com.icare.model.PatientModel
 import com.icare.model.PharmacistsModel
-import com.icare.model.PharmacyModel
 import com.icare.model.ResponseModel
 import com.icare.model.TokenRequest
 import com.icare.model.Users
 import com.icare.repository.UserRepository
-import com.icare.utils.EMPTY_LIST
-import com.icare.utils.FAILED
 import com.icare.utils.INVALID_TOKEN
 import com.icare.utils.INVALID_USER
 import com.icare.utils.OK
