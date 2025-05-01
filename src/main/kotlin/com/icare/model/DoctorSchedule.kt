6bf3a75d49596d6bc859762491ba4b5553f951c3
Ali Mansour <dev.ali.mansour@gmail.com>
2025-05-01 17:26:11 +0300

    Add additional properties to DoctorSchedule for rating and time management

--- src/main/kotlin/com/icare/model/DoctorSchedule.kt
+++ src/main/kotlin/com/icare/model/DoctorSchedule.kt
@@ -4,6 +4,9 @@ data class DoctorSchedule(
     val totalPatients: Long = 0,
     val confirmed: Long = 0,
     val price: Double = 0.0,
+    val rating: Double = 0.0,
+    val fromTime: Long = 0,
+    val toTime: Long = 0,
     val availableSlots: Short = 0,
     val appointments: List<Appointment> = emptyList(),
 )
