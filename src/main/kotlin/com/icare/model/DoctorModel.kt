09f43af9c9949a041ed6d9ff2ff16e0315c48b3c
Ali Mansour <dev.ali.mansour@gmail.com>
2025-05-01 17:25:19 +0300

    Format DoctorModel properties for improved readability and consistency

--- src/main/kotlin/com/icare/model/DoctorModel.kt
+++ src/main/kotlin/com/icare/model/DoctorModel.kt
@@ -1,19 +1,19 @@
 package com.icare.model
 
 data class DoctorModel(
-    val token:String="",
-    val doctorID:String="",
-    val fname:String="",
-    val lname:String="",
-    val email:String="",
-    val isActive:Boolean=true,
-    val specialization:String="",
-    val fromTime:Long=0,
-    val toTime:Long=0,
-    val clinicId:String="",
+    val token: String = "",
+    val doctorID: String = "",
+    val fname: String = "",
+    val lname: String = "",
+    val email: String = "",
+    val isActive: Boolean = true,
+    val specialization: String = "",
+    val fromTime: Long = 0,
+    val toTime: Long = 0,
+    val clinicId: String = "",
     val phoneNumber: String = "",
-    val rating:Double=0.0,
-    val price:Double=0.0,
-    val profilePicture:String="",
+    val rating: Double = 0.0,
+    val price: Double = 0.0,
+    val profilePicture: String = "",
 
-)
+    )
